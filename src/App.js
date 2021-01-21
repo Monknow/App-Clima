126f5a848e54374ff87047f58f634e8c8a23cff7
Juan <juanrodriguezcode.com>
2021-01-21 10:44:28 -0400

    Se ha extraido los datos de la api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import React, {useState, useEffect}  from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const traerDatos = async () => {
+  return await axios
+    .get(
+      "http://api.openweathermap.org/data/2.5/weather?q=Caracas&lang=es&units=metric&appid=cfdf46ac4ccb5e3a4775488af2e57ce0"
+    )
+    .then((res) => {
+      console.log(res.data);
+      return res.data;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+
+function DatosBasicos() {
+
+  const [clima, setClima] = useState(null);
+  const [cargando, setCargando] = useState(true);
+
+
+  useEffect(() =>{
+    const montarDatos = async () => {
+      const resultado = await traerDatos();
+      console.log(resultado);
+      setClima(resultado);
+      setCargando(false);
+    }
+
+    montarDatos();
+  }, [])
+
+    return (
+      <div> 
+        {cargando || !clima ? (
+          <h1>Cargando...</h1>
+        ):(
+        <div>
+          <h1>{clima.name}</h1>
+          <h2>{`${clima.main.temp}°`}</h2>
+          <h3>{`${Math.floor(clima.main.temp_max)}°`}/{`${Math.floor(clima.main.temp_min)}°`}</h3>
+        </div>
+        )}
+      </div>
+    );
+  
+
 }
 
-export default App;
+export default DatosBasicos;
